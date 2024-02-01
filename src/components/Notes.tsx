9f89659bcbe49cf7c103356576dabadf0ce75c69
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-02-01 15:42:08 +0000

    notes background

---
+++ src/components/Notes.tsx
@@ -0,0 +1,7 @@
+export default function Notes() {
+  return (
+    <div
+      className={`w-5/6 h-5/6 dark:bg-white/[0.2] bg-gray-200/[0.3] backdrop-blur-[1px] rounded-xl drop-shadow-3xl b-shadow`}
+    ></div>
+  );
+}
