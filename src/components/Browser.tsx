c93612b5b08b36258a23a287bcb3c737ab3ced11
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-02-09 10:51:21 +0000

    fix: stackability of elements

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -27,66 +27,72 @@ export default function Browser({
   return (
     <Draggable handle=".handle">
       <div
-        className={
-          browserOpen
-            ? browserMinimised && !browserExpanded
-              ? "hidden-anim mx-2 h-[calc(100%-79px-2.5rem)] w-full xl:w-4/6 sm:h-4/6 backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8] absolute sm:relative"
-              : browserExpanded
-              ? browserMinimised
-                ? "hidden-anim w-full mx-0 h-[calc(100%-79px-2.5rem)] sm:h-full backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8] center absolute sm:relative"
-                : "mx-0 h-[calc(100%-79px-2.5rem)] sm:h-full w-full backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8] center absolute sm:relative"
-              : " xl:w-4/6 mx-2 h-[calc(100%-79px-2.5rem)] w-full sm:h-4/6 backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8]  absolute sm:relative"
-            : "hidden"
-        }
+        className={` ${
+          browserMinimised && "z-[-1]"
+        } flex justify-center sm:items-center absolute w-content h-content z-20`}
       >
-        <Media queries={{ small: { maxWidth: 599 } }}>
-          {(matches) =>
-            matches.small ? (
-              <div className="flex flex-row bg-[#ccd4e8]  w-full h-[2.5rem] rounded-t-lg">
-                <Buttons
-                  setWindowOpen={setBrowserOpen}
-                  setWindowMinimised={setBrowserMinimised}
-                  setWindowExpanded={setBrowserExpanded}
-                  windowExpanded={browserExpanded}
-                />
-              </div>
-            ) : (
-              <>
-                <div className="flex flex-row bg-[#ccd4e8] dark:bg-slate-900 w-full h-[2.5rem] rounded-t-lg">
-                  <span className="handle w-[calc(100%-5rem)] col-span-3 h-10 ml-20 absolute" />
+        <div
+          className={
+            browserOpen
+              ? browserMinimised && !browserExpanded
+                ? "hidden-anim mx-2 backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8] absolute sm:relative"
+                : browserExpanded
+                ? browserMinimised
+                  ? "hidden-anim backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8] center absolute sm:relative"
+                  : "mx-0 backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8] center absolute sm:relative"
+                : "w-screen h-[calc(100vh-7rem)] sm:h-5/6 sm:w-[70rem] sm:mx-2 backdrop-blur-[1px] rounded-lg drop-shadow-3xl h-shadow border-[0.3px] border-gray-400 dark:bg-slate-900 bg-white/[0.8] sm:relative"
+              : "hidden z-[-1]"
+          }
+        >
+          <Media queries={{ small: { maxWidth: 599 } }}>
+            {(matches) =>
+              matches.small ? (
+                <div className="flex flex-row bg-[#ccd4e8] w-full h-[2.5rem] rounded-t-lg">
                   <Buttons
                     setWindowOpen={setBrowserOpen}
                     setWindowMinimised={setBrowserMinimised}
                     setWindowExpanded={setBrowserExpanded}
                     windowExpanded={browserExpanded}
                   />
-                  <BrowserTabs
-                    activeTab={activeTab}
-                    setActiveTab={setActiveTab}
-                  />
                 </div>
-                <div className="bg-blue-200 dark:bg-blue-900 w-full h-[2.5rem] z-30 relative top-shadow">
-                  <SearchBar activeTab={activeTab} />
-                </div>
-              </>
-            )
-          }
-        </Media>
+              ) : (
+                <>
+                  <div className="flex flex-row bg-[#ccd4e8] dark:bg-slate-900 w-full h-[2.5rem] rounded-t-lg">
+                    <span className="handle w-[calc(100%-5rem)] col-span-3 h-10 ml-20 absolute" />
+                    <Buttons
+                      setWindowOpen={setBrowserOpen}
+                      setWindowMinimised={setBrowserMinimised}
+                      setWindowExpanded={setBrowserExpanded}
+                      windowExpanded={browserExpanded}
+                    />
+                    <BrowserTabs
+                      activeTab={activeTab}
+                      setActiveTab={setActiveTab}
+                    />
+                  </div>
+                  <div className="bg-blue-200 dark:bg-blue-900 w-full h-[2.5rem] z-30 relative top-shadow">
+                    <SearchBar activeTab={activeTab} />
+                  </div>
+                </>
+              )
+            }
+          </Media>
 
-        <Media queries={{ small: { maxWidth: 599 } }}>
-          {(matches) =>
-            matches.small ? (
-              <>
-                <BrowserMenu setShowTabs={setShowTabs} showTabs={showTabs} />
-                <div className="w-full bg-[#ccd4e8] dark:bg-slate-600 h-[3rem] absolute bottom-10 top-shadow">
-                  <SearchBar activeTab={activeTab} />
-                </div>
-              </>
-            ) : null
-          }
-        </Media>
+          <Media queries={{ small: { maxWidth: 599 } }}>
+            {(matches) =>
+              matches.small ? (
+                <>
+                  <BrowserMenu setShowTabs={setShowTabs} showTabs={showTabs} />
+                  <div className="w-full bg-[#ccd4e8] dark:bg-slate-600 h-[3rem] absolute bottom-10 top-shadow">
+                    <SearchBar activeTab={activeTab} />
+                  </div>
+                </>
+              ) : null
+            }
+          </Media>
 
-        {showTabs ? <TabsContainer /> : null}
+          {showTabs ? <TabsContainer /> : null}
+        </div>
       </div>
     </Draggable>
   );
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -21,52 +21,53 @@ export default function Notes({
   const [noteClicked, setNoteClicked] = useState<number>(1);
   const [noteExpanded, setNoteExpanded] = useState<boolean>(false);
 
-  console.log(noteExpanded);
   return (
     <Draggable handle=".handle">
-      <div
-        className={
-          notepadOpen
-            ? noteMinimised && !noteExpanded
-              ? "hidden-anim xl:w-4/6 xl:h-4/6 h-[calc(100%-79px-2rem)] sm:h-4/6 backdrop-blur-[2px] rounded-lg drop-shadow-3xl h-shadow grid grid-cols-3 border-[0.3px] border-gray-400"
-              : noteExpanded
-              ? noteMinimised
-                ? "hidden-anim w-full h-[calc(100%-79px-2rem)] sm:h-full backdrop-blur-[2px]] rounded-lg drop-shadow-3xl h-shadow grid grid-cols-3 border-[0.3px] border-gray-400 center"
-                : "mx-0 h-[calc(100%-79px-2rem)] w-full sm:h-full backdrop-blur-[2px] rounded-lg grid grid-cols-3 border-[0.3px] border-gray-400 center"
-              : "xl:w-4/6 xl:h-4/6 sm:h-4/6 h-[calc(100%-79px-2rem)] w-full backdrop-blur-[2px] rounded-lg drop-shadow-3xl h-shadow grid grid-cols-3 border-[0.3px] border-gray-400"
-            : "hidden"
-        }
-      >
-        <span className="handle w-[calc(100%-6rem)] col-span-3 h-10 ml-24 absolute" />
-        <div className="col-span-1 dark:bg-slate-800/[0.9] bg-blue-100/[0.9] backdrop-blur-[2px] rounded-l-lg border-r-[1px] border-gray-500 max-w-full">
+      <div className="flex justify-center sm:items-center absolute">
+        <div
+          className={
+            notepadOpen
+              ? noteMinimised && !noteExpanded
+                ? "hidden-anim xl:w-4/6 xl:h-4/6 h-[calc(100%-79px-2rem)] sm:h-4/6 backdrop-blur-[2px] rounded-lg drop-shadow-3xl h-shadow grid grid-cols-3 border-[0.3px] border-gray-400"
+                : noteExpanded
+                ? noteMinimised
+                  ? "hidden-anim w-full h-[calc(100%-79px-2rem)] sm:h-full backdrop-blur-[2px]] rounded-lg drop-shadow-3xl h-shadow grid grid-cols-3 border-[0.3px] border-gray-400 center"
+                  : "mx-0 h-[calc(100%-79px-2rem)] w-full sm:h-[calc(100vh-7.5rem)] backdrop-blur-[2px] rounded-lg grid grid-cols-3 border-[0.3px] border-gray-400 center"
+                : "xl:w-4/6 xl:h-4/6 sm:h-4/6 h-[calc(100%-79px-2rem)] w-full backdrop-blur-[2px] rounded-lg drop-shadow-3xl h-shadow grid grid-cols-3 border-[0.3px] border-gray-400"
+              : "hidden"
+          }
+        >
           <span className="handle w-[calc(100%-6rem)] col-span-3 h-10 ml-24 absolute" />
-          <Buttons
-            setWindowOpen={setNotepadOpen}
-            setWindowExpanded={setNoteExpanded}
-            windowExpanded={noteExpanded}
-            setWindowMinimised={setNoteMinimised}
-          />
+          <div className="col-span-1 dark:bg-slate-800/[0.9] bg-blue-100/[0.9] backdrop-blur-[2px] rounded-l-lg border-r-[1px] border-gray-500 max-w-full">
+            <span className="handle w-[calc(100%-6rem)] col-span-3 h-10 ml-24 absolute" />
+            <Buttons
+              setWindowOpen={setNotepadOpen}
+              setWindowExpanded={setNoteExpanded}
+              windowExpanded={noteExpanded}
+              setWindowMinimised={setNoteMinimised}
+            />
 
-          <input
-            className="w-[95%] h-8 bg-white rounded-lg block border-[0.5px] border-gray-500 mb-2 dark:bg-slate-400 text-base sm:text-sm"
-            placeholder="Search"
-          />
-          <ul>
-            {NoteInfo.map((note) => {
-              return (
-                <li key={note.id}>
-                  <IndivNote
-                    note={note}
-                    setNoteClicked={setNoteClicked}
-                    noteClicked={noteClicked}
-                  />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className="col-span-2 dark:bg-slate-900 bg-gray-200 rounded-r-lg">
-          <IndivNoteBody noteClicked={noteClicked} />
+            <input
+              className="w-[95%] h-8 bg-white rounded-lg block border-[0.5px] border-gray-500 mb-2 dark:bg-slate-400 text-base sm:text-sm"
+              placeholder="Search"
+            />
+            <ul>
+              {NoteInfo.map((note) => {
+                return (
+                  <li key={note.id}>
+                    <IndivNote
+                      note={note}
+                      setNoteClicked={setNoteClicked}
+                      noteClicked={noteClicked}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="col-span-2 dark:bg-slate-900 bg-gray-200 rounded-r-lg">
+            <IndivNoteBody noteClicked={noteClicked} />
+          </div>
         </div>
       </div>
     </Draggable>
