182f91155ac0fdeb7ce81f2cf65840f2bc3ae018
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-02-16 15:51:43 +0000

    style video for mobile

--- src/components/DuckVideos.tsx
+++ src/components/DuckVideos.tsx
@@ -1,20 +1,39 @@
 import ReactPlayer from "react-player";
 import youtubeLogo from "../assets/youtube-logo.png";
+import Media from "react-media";
 
 export default function DuckVideos() {
   return (
-    <div className="w-fit h-[280px] rounded-md border-[1px] border-gray-200 rounded-md b-shadow  overflow-hidden">
-      <ReactPlayer
-        url="https://youtu.be/OJvwS6T8qVw"
-        width="300px"
-        height="200px"
-        controls={true}
-      />
+    <div className="sm:w-fit h-[230px] mx-2 sm:ml-0 sm:h-[280px] rounded-md border-[1px] border-gray-200 rounded-md b-shadow overflow-hidden flex flex-col">
+      <Media queries={{ small: { maxWidth: 599 } }}>
+        {(matches) =>
+          matches.small ? (
+            <div className="self-center">
+              <ReactPlayer
+                url="https://youtu.be/OJvwS6T8qVw"
+                width="100vw"
+                height="180px"
+                controls={true}
+              />
+            </div>
+          ) : (
+            <ReactPlayer
+              url="https://youtu.be/OJvwS6T8qVw"
+              width="300px"
+              height="200px"
+              controls={true}
+            />
+          )
+        }
+      </Media>
+
       <a href="https://youtu.be/OJvwS6T8qVw" target="_blank">
-        <h4 className="mx-4 mt-2 font-medium">SideQuest Demo </h4>
-        <div className="flex flex-row text-xs ml-4 mt-3">
-          <img src={youtubeLogo} alt="youtube logo" className="size-5" />
-          <span className="self-center">YouTube</span>
+        <div className="sm:block flex flex-cols justify-between mx-4 sm:mx-0">
+          <h4 className="sm:mx-4 mt-2 font-medium">SideQuest Demo </h4>
+          <div className="flex flex-row text-xs ml-4 mt-3">
+            <img src={youtubeLogo} alt="youtube logo" className="size-5" />
+            <span className="self-center">YouTube</span>
+          </div>
         </div>
       </a>
     </div>
